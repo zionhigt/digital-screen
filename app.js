c886ba83a14390edd07259889ad42f6309067a61
zionhigt <larcher501@gmail.com>
2024-05-11 09:02:36 +0200

    Config api

--- app.js
+++ app.js
@@ -1,34 +1,29 @@
 const express = require("express");
 
 const config = require("./config.json");
+const credential = require("./credential.json");
 const app = express();
-
 
 app.use("/", express.static("./public"));
 
 const { Api } = require("./lib/api.js");
 app.api = Api({
-    base: "http://localhost:3000",
+    base: "https://api.meteo-concept.com/api",
     root: config.cache_path || "./data",
+    ...credential,
 });
 
-app.get("/test", function(req, res) {
-    
-    app.api.get("/test_cache")
+app.get("/forecast/:city", function(req, res) {
+    const city = req.params.city;
+    app.api.searchCity(city)
     .then(response => {
-        console.log(response)
-        res.status(200).json(response);
+        res.status(200).json(app.api.transformForecast(response));
     })
     .catch(err => {
         console.error(err);
         res.status(500).json(err);
     })
 
-})
-app.get("/test_cache", function(req, res) {
-    res.status(200).json({
-        test: "OK"
-    })
 })
 
 module.exports = app;
